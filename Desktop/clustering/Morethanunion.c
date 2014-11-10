c246c41ab66f924a078faddcd960fb9fe72ea2bc
Di Xu <sirxudi@gmail.com>
2014-11-09 19:22:07 -0500

    ADD moreunion

---
+++ Desktop/clustering/Morethanunion.c
@@ -0,0 +1,34 @@
+/*this code use union find method to */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+
+#include "readin.h"
+#include "union_find.h"
+#include "bridges.h"
+void main()
+{
+  int i,j;
+  clock_t time;
+  SHEET *clay=(SHEET*)malloc(sizeof(SHEET)*N_CLAY);
+  POLY *chain=(POLY*)malloc(sizeof(POLY)*N_CHAIN);
+  FILE *f_filler=fopen("filler.lammpstrj","r");
+  FILE *f_chain=fopen("chains.lammpstrj","r");
+  int n_entries=0;
+  clearbridges();
+  systeminfo();
+  while(!feof(f_filler)&&!feof(f_chain)&&n_entries<ENTRIES_BEFORESHEAR){
+    time=clock();
+    if(feof(f_filler)||feof(f_chain)){printf("ERROR: Two file not of same length, or reading speed not sync\n");exit(17);}
+    clearinfo(clay,chain);
+    readfiller(f_filler,clay);
+    readchain(f_chain,chain);
+    bridges(clay,chain);
+    findunion(clay,chain);
+    n_entries++;
+    printf("TIME COST: %f \n",((float)(clock()-time))/CLOCKS_PER_SEC);
+    //getchar();
+  }
+}
